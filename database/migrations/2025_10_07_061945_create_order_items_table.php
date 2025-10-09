3f409f9e948c1dfd6674f058cfab9c065d52c42a
JushLeHuuTin <jushlehuutinads@gmail.com>
2025-10-09 15:09:03 +0700

    add seeder

--- database/migrations/2025_10_07_061945_create_order_items_table.php
+++ database/migrations/2025_10_07_061945_create_order_items_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->string('order_item_id', 50)->unique();
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
