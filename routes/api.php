c0d879b0984cc680e5434386aa7adb8fffcbaf9c
JushLeHuuTin <jushlehuutinads@gmail.com>
2025-10-14 14:16:20 +0700

    prepare f4/view-product

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Models\Category;
 
-Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products', [ProductController::class, 'index']);
 // Các route yêu cầu phải đăng nhập thì cho vào group này
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/products', [ProductController::class, 'store']);
     
     Route::delete('/products/{product}', [ProductController::class, 'destroy']);
     Route::put('/products/{product}', [ProductController::class, 'update']);
