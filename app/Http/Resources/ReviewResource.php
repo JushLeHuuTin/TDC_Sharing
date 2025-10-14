ff82d63bcebf0b613d6faa480dd1f50e4aca91ab
hanh07082005 <hanh2005k@gmail.com>
2025-10-14 13:38:22 +0700

    prepare

---
+++ app/Http/Resources/ReviewResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'             => $this->id,
+            // Lấy tên người dùng, nếu không có (VD: user đã bị xóa) thì trả về 'Người dùng ẩn danh'
+            'reviewer_name'  => $this->whenLoaded('user', $this->user->name ?? 'Người dùng ẩn danh'),
+            'rating'         => $this->rating,
+            'comment'        => $this->comment,
+            // Định dạng ngày tháng cho dễ đọc (VD: "2 ngày trước")
+            'created_at'     => $this->created_at->diffForHumans(),
+            'created_at_raw' => $this->created_at->toDateTimeString(),
+        ];
+    }
+}
