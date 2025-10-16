4babfb92ca6c8213a6fd678910d1e46730205f47
Dong dep trai <dong12062004@gmail.com>
2025-10-16 13:14:33 +0700

    dong add order

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\VoucherController;
+use App\Http\Controllers\OrderController;
 
 Route::post('/products', [ProductController::class, 'store']);
 // Các route yêu cầu phải đăng nhập thì cho vào group này
@@ -12,4 +13,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/products/{product}', [ProductController::class, 'destroy']);
     Route::post('/cart/add', [CartController::class, 'addItem'])->name('cart.add');
     Route::post('/vouchers', [VoucherController::class, 'store']);
+     // API tạo đơn hàng sau khi thanh toán
+    Route::post('/orders', [OrderController::class, 'store']);
+    
+    // API xem chi tiết đơn hàng (có kiểm tra bảo mật)
+    Route::get('/orders/{id}', [OrderController::class, 'show']); 
 });
