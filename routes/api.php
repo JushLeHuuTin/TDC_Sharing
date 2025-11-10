3ddf76279b4ffe58635fd3ecb124564ca4fffe0d
JushLeHuuTin <jushlehuutinads@gmail.com>
2025-11-10 13:14:32 +0700

    tin delete all f3

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
-Route::post('/products', [ProductController::class, 'store']);
-// Các route yêu cầu phải đăng nhập thì cho vào group này
-Route::middleware('auth:sanctum')->group(function () {
-    
-    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
-});
