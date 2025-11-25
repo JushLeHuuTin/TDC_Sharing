1b0bfcece95846646151ed6b9422bdca92b4fc7f
hanh07082005 <hanh2005k@gmail.com>
2025-11-25 14:44:18 +0700

    chay thanh cong

--- backend/vendor/autoload.php
+++ backend/vendor/autoload.php
@@ -14,12 +14,9 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    trigger_error(
-        $err,
-        E_USER_ERROR
-    );
+    throw new RuntimeException($err);
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc514d8f7b9fc5970bdd94287905ef584::getLoader();
+return ComposerAutoloaderInitfa5739a68a391a79456428bed2f94df4::getLoader();
